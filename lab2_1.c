23533dc5ced789eef733d809cfa7f9e616f64928
b6320500549 <jirasin.po@ku.th>
2021-02-08 18:03:41 +0700

    Update lab2_1.c

--- lab2_1.c
+++ lab2_1.c
@@ -2,35 +2,28 @@
 int main()
 {
   int a,b,c,sum;
-  printf("Enter a:");
   scanf("%d",&a);
-  printf("Enter b:");
   scanf("%d",&b);
-  printf("Enter c:");
   scanf("%d",&c);
   if(a>=0&&a<=30||b>=0&&b<=30||c>=0&&c<=40)
   {
    sum = a+b+c;
   if(sum>=80)
-    printf("A");
-  else if(sum>=75)
-    printf("B+");
-  else if(sum>=75)
-    printf("B+");
-  else if(sum>=70)
-    printf("B");
-  else if(sum>=65)
-    printf("C+");
-  else if(sum>=60)
-    printf("C");
-  else if(sum>=55)
-    printf("D");
-  else if(sum>=50)
-    printf("D+");
-  else printf("F");
-
+  printf("A") ;
+  else if (sum>=75&&sum<=79)
+  printf("B+") ;
+  else if (sum>=70&&sum<=74)
+  printf("B") ;
+  else if (sum>=65&&sum<=69)
+  printf("C+") ;
+  else if (sum>=60&&sum<=64)
+  printf("C") ;
+  else if (sum>=55&&sum<=59)
+  printf("D+") ;
+  else if (sum>=50&&sum<=54)
+  printf("D") ;
+  else if (sum>=0&&sum<=49)
+  printf("F") ;
   }
-
-
   return 0;
 }
