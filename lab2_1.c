937ae558da2c21a543ce65e4ea70a3933c72e7a5
b6320500549 <jirasin.po@ku.th>
2021-02-08 17:56:48 +0700

    Update lab2_1.c

--- lab2_1.c
+++ lab2_1.c
@@ -4,15 +4,11 @@ int main()
   int a,b,c,sum;
   printf("Enter a:");
   scanf("%d",&a);
-  if(a>=0&&a<=30)
-  {
   printf("Enter b:");
   scanf("%d",&b);
-   if(b>=0&&b<=30)
-  {
   printf("Enter c:");
   scanf("%d",&c);
-   if(c>=0&&c<=40)
+  if(a>=0&&a<=30||b>=0&&b<=30||c>=0&&c<=40)
   {
    sum = a+b+c;
   if(sum>=80)
@@ -32,9 +28,7 @@ int main()
   else if(sum>=50)
     printf("D+");
   else printf("F");
-  }
 
-  }
   }
 
 
