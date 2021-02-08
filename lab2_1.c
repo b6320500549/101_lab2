9fa4b384cbe3b1bb34958d48e704df29332d771f
b6320500549 <jirasin.po@ku.th>
2021-02-08 17:36:23 +0700

    Initial commit

--- lab2_1.c
+++ lab2_1.c
@@ -4,11 +4,39 @@ int main()
   int a,b,c,sum;
   printf("Enter a:");
   scanf("%d",&a);
+  if(a>=0&&a<=30)
+  {
   printf("Enter b:");
   scanf("%d",&b);
+   if(a>=0&&a<=30)
+  {
   printf("Enter c:");
   scanf("%d",&c);
-  sum = a+b+c;
+   if(a>=0&&a<=40)
+  {
+   sum = a+b+c;
+  if(sum>=80)
+    printf("A");
+  else if(sum>=75)
+    printf("B+");
+  else if(sum>=75)
+    printf("B+");
+  else if(sum>=70)
+    printf("B");
+  else if(sum>=65)
+    printf("C+");
+  else if(sum>=60)
+    printf("C");
+  else if(sum>=55)
+    printf("D");
+  else if(sum>=50)
+    printf("D+");
+  else printf("F");
+  }
+
+  }
+  }
+
 
   return 0;
 }
